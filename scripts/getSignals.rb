27290eb8b35e1163cdacf655384f416241aec78f
Joerg T. (Mic92) <jthalheim@gmail.com>
2011-07-17 17:43:47 +0200

    Add pretty print option

--- scripts/getSignals.rb
+++ scripts/getSignals.rb
@@ -1,39 +1,48 @@
 #!/usr/bin/ruby
 
+require 'json'
+require 'optparse'
+require 'iconv'
+
 class Parser
 	class Dataset
 		attr_accessor :name, :signals, :sendid
 		def initialize()
 			@signals = []
 		end
-		def to_s()
-		return <<EOF
-	// Send-ID #{@sendid}
-	new SignalGroup(#{@name},
-	new Signal[]{
-#{@signals.join.chomp}
-	}),
-EOF
+
+		def to_json(*a)
+			return {'dataset' => @name,
+				'sendid'  => @sendid, 
+				'signals' => @signals
+			}.to_json(*a)
 		end
 	end
 
 	class Signal
 		attr_accessor :name, :type, :unit, :factor
-		def to_s()
-			return <<-EOF 
-		new Signal("#{@name}", "#{@unit}", #{@factor}, Signal.Size.#{@type}),
-			EOF
+		def to_json(*a)
+			return {'name'  => @name,
+				'unit'  => @unit,
+				'factor'=> @factor,
+				'type'  => @type
+			}.to_json(*a)
 		end
 	end
+
 	def initialize()
-		@state = :close
 		@sets = []
 		@set = nil
 	end
 
 	def parse(file)
 		@state = :close
-		File.readlines(file).each do |line|
+		file_handle = File.open(file)
+		# To handle invalid byte sequence right.
+		ic = Iconv.new('UTF-8//IGNORE', 'UTF-8')
+		
+		while(input = file_handle.gets)
+			line = ic.iconv(input)
 			case @state
 				when :close 
 					next unless (line =~ /.*#Dataset_START#.*/)
@@ -50,18 +59,21 @@ EOF
 						@set.sendid = @sets.length + 1
 					when /.*uart_write_(?:int|long)\(([^)]+)\)/
 						signal = Signal.new
-						signal.name = $1.sub!(/_([^_]+)$/, '')
+						# dirty cut of expression which are not var names
+						signal.name = $1.sub(/[^A-z0-9_\[\]]+.*$/, "") 
+						signal.name =~ /_([ilcus]{2})/
 						signal.type = case $1
 							      	when /sc/ then "CHAR"
 							      	when /uc/ then "UCHAR"
 							      	when /si/ then "INT"
 							      	when /ui/ then "UINT"
 							      	when /sl/ then "LONG"
-							      	else "INT"
+							      	else "INT" 
 							      end
 						
 						/.*Unit:#([^#]+)#.*/ =~ line
-						signal.unit = $1
+						# Empty unit field
+						signal.unit = ($1 == nil ? "-" : $1)
 						/.*Factor:#([^#]+)#.*/ =~ line
 						factor = $1.tr(",", ".") 
 						/.*Divisor:#([^#]+)#.*/ =~ line
@@ -74,30 +86,41 @@ EOF
 					end
 			end
 		end
-			return @sets
+		file_handle.close()
+		return @sets
 	end
 end
 
-if ARGV.empty? then
-	puts <<-EOF
-Parse communication.c of the Skyquad source code 
-and generates java code, which represent its protocol 
-usage:
-	#{File.basename $0} communication.c
+pretty_print = false
+optparse = OptionParser.new do|opts|
+	opts.banner = <<-EOF
+Usage: #{File.basename $0} [options] source1.c source2.c ..."
+Parse communication.c of the Skyquad source code and
+generates json, which represent its protocol.
 	EOF
+
+	opts.on( '-pp', '--prettyprint', 'Prints json human readable' ) do
+		pretty_print = true
+	end
+
+	opts.on( '-h', '--help', 'Display this screen' ) do
+		puts opts
+		exit
+	end
 end
+optparse.parse!
 
 ARGV.each do |file|
 	if !File.exist?(file) then
-		puts "'Skip #{file}: it does not exist!'"
+		puts "'Skip file #{file}: it does not exist!'"
 		next
 	end
 	p = Parser.new
 	sets = p.parse(file)
-	puts <<EOF
-public static final SignalGroup[] Db = new SignalGroup[]{"
-#{sets.join}
-};
-EOF
+	if pretty_print then 
+		puts JSON.pretty_generate(sets)
+	else
+		puts JSON.dump(sets)
+	end
 end
 #vim: foldmethod=marker:filetype=ruby:expandtab:shiftwidth=2:tabstop=2:softtabstop=2:textwidth=80
